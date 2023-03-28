d5627fb809c94ca3c237c18b8b671120e82e40d2
crispyparmaham <80284494+crispyparmaham@users.noreply.github.com>
2023-03-28 12:31:23 +0200

    adding functionality to clear button, to clear all todos that checked

--- project/js/app.js
+++ project/js/app.js
@@ -92,5 +92,12 @@
 
         // clear all completed toDos on clear button
 
-        
+        const clearBtn = document.querySelector(".clear-completed")
+            
+            clearBtn.addEventListener("click", () => {
+                const completedToDos = todoList.querySelectorAll(".completed")
+                for(const completedToDo of completedToDos) {
+                    completedToDo.remove()
+                }
+            })
     });
